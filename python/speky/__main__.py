2ab34af1f9b4c86e3ff77466a2eee0f607eb7225
Antoine <angagnie@student.42.fr>
2025-09-24 22:35:20 +0200

    Add a CLI arg to disable sorting requirements

--- python/speky/__main__.py
+++ python/speky/__main__.py
@@ -64,7 +64,15 @@ def main():
         default=default_logging_file,
         help='Specify a custom config file of the logging library',
     )
-    cli_parser.add_argument('-c', '--check-only', action='store_true')
+    cli_parser.add_argument(
+        '-c', '--check-only', action='store_true', help='Validate input files but do not output any markdown'
+    )
+    cli_parser.add_argument(
+        '--sort',
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help='Sort requirements by ID. If false, the order of files passed as positionals is significant',
+    )
     cli_args = cli_parser.parse_args()
 
     logging_config_file = Path(cli_args.logging_config)
@@ -85,7 +93,7 @@ def main():
         logger.critical(err)
         sys.exit(1)
     if not cli_args.check_only:
-        specification_to_myst(specs, cli_args.project_name, cli_args.output_folder)
+        specification_to_myst(specs, cli_args.project_name, cli_args.output_folder, cli_args.sort)
 
 
 def import_fields(destination, source: dict, fields: list[str]):
