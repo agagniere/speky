5f1a7e01dd116fd14f7b2e6db187e7092e567bfc
Antoine GAGNIERE <antoine.gagniere@datadoghq.com>
2026-04-06 21:22:30 +0200

    Add a manifest for test samples

--- tests/test_mcp_nominal.py
+++ tests/test_mcp_nominal.py
@@ -16,13 +16,4 @@ def test_simple_sample(sample):
 
 def test_more_samples(sample):
     with patch('speky_mcp.server.run_server'):
-        speky_mcp.server.run(
-            [
-                sample('simple_requirements'),
-                sample('simple_comments'),
-                sample('simple_tests'),
-                sample('more_requirements'),
-                sample('more_comments'),
-                sample('more_tests'),
-            ]
-        )
+        speky_mcp.server.run([sample('more_samples')])
